11947a2964cfaeb4c3a72a271ff10bff8199c9d6
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2022-06-27 16:16:54 +0300

    fix: ethers helper typings

--- helpers/ethers.ts
+++ helpers/ethers.ts
@@ -1,6 +1,7 @@
 import { getAddress } from '@ethersproject/address';
 import { AddressZero } from '@ethersproject/constants';
 import { Contract } from '@ethersproject/contracts';
+import { Web3Provider } from '@ethersproject/providers';
 
 export function isAddress(value: string) {
   try {
@@ -10,18 +11,23 @@ export function isAddress(value: string) {
   }
 }
 
-export function getSigner(library: any, account: any) {
-  return library.getSigner(account).connectUnchecked();
+export function getSigner(web3Provider: Web3Provider, address: string) {
+  return web3Provider.getSigner(address).connectUnchecked();
 }
 
-export function getProviderOrSigner(library: any, account: any) {
-  return account ? getSigner(library, account) : library;
+export function getProviderOrSigner(web3Provider: Web3Provider, address: string) {
+  return address ? getSigner(web3Provider, address) : web3Provider;
 }
 
-export function getContract(address: string, ABI: any, library: any, account: any) {
-  if (!isAddress(address) || address === AddressZero) {
-    throw Error(`Invalid 'address' parameter '${address}'.`);
+export function getContract(
+  contractAddress: string,
+  ABI: any,
+  web3Provider: Web3Provider,
+  providerAddress: string
+) {
+  if (!isAddress(contractAddress) || contractAddress === AddressZero) {
+    throw Error(`Invalid 'address' parameter '${contractAddress}'.`);
   }
 
-  return new Contract(address, ABI, getProviderOrSigner(library, account));
+  return new Contract(contractAddress, ABI, getProviderOrSigner(web3Provider, providerAddress));
 }
