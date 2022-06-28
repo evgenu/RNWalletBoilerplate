b27f8d428f5b5a084a4f59baefa2cf0ce6b729e3
evgenu <evgenyyyiii@gmail.com>
2022-06-28 15:18:51 +0300

    [Update] Transaction with error handling

--- helpers/ethers.ts
+++ helpers/ethers.ts
@@ -2,6 +2,7 @@ import { getAddress } from '@ethersproject/address';
 import { AddressZero } from '@ethersproject/constants';
 import { Contract } from '@ethersproject/contracts';
 import { Web3Provider } from '@ethersproject/providers';
+import { Alert } from 'react-native';
 
 export function isAddress(value: string) {
   try {
@@ -30,4 +31,18 @@ export function getContract(
   }
 
   return new Contract(contractAddress, ABI, getProviderOrSigner(web3Provider, providerAddress));
+}
+
+export async function sendTransaction(
+  functionToExecute: Function,
+  triesToExecute: number = 0
+) {
+  const functionTransaction = await functionToExecute();
+  const functionReceipt = await functionTransaction.await();
+
+  if(functionReceipt !== 1 && triesToExecute === 3) {
+    Alert.alert('Transaction Failure', 'Could not send transaction', [{text: 'OK', style: 'cancel'}]);
+  } else if(functionReceipt !== 1) {
+    sendTransaction(functionToExecute, triesToExecute = triesToExecute + 1);
+  }
 }
