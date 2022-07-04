dc0e7d57b8a737ab79686446d7f0dbeaac9799d3
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2022-07-04 11:23:53 +0300

    fix: use default metro config

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  resolver: {
-    extraNodeModules: require("expo-crypto-polyfills"),
-  },
-};
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('@expo/metro-config');
+
+// extra config is needed to enable `expo-crypto-polyfills`
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  return {
+    ...defaultConfig,
+    resolver: {
+      extraNodeModules: {
+        ...defaultConfig.resolver.extraNodeModules,
+        ...require('expo-crypto-polyfills'),
+      },
+    },
+  };
+})();
