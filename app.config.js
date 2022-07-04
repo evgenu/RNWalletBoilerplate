bd77b0b5775d6fb3b5f5abb842071cb05f007a58
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2022-07-04 11:23:19 +0300

    fix: change app json to app config js file

---
+++ app.config.js
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+
+export default {
+  name: 'WalletConnectExample',
+  slug: 'WalletConnectExample',
+  scheme: 'walletconnect-example',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+    package: 'contract.test',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+};
